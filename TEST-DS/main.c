e6bc237b3628ea609369c83cb4a22c78b02e8e8b
Rahul Mahajan <rahul.mahajan@exilant.com>
2017-02-16 17:14:35 +0530

    1. Added Linked List Operations

--- TEST-DS/main.c
+++ TEST-DS/main.c
@@ -17,7 +17,6 @@ struct ListNode{
     struct ListNode *next;
 }*head;
 
-
 _Bool isListEmpty(){
     if (head==NULL) {
         return 1;
@@ -76,7 +75,7 @@ void displayList(){
             printf("NULL");
         }
     }
-     printf("\n\n");
+    printf("\n\n");
 }
 
 void insertAt(int position,int data){
@@ -128,42 +127,267 @@ void deleteNode(int data){
 
 void reverseList(){
     if (!isListEmpty()) {
-    struct ListNode *prev=NULL, *current = head , *next = NULL;
-    
-    while (current!=NULL){
-        next = current->next;
-        current->next= prev;
-        prev =current;
-        current = next;
+        struct ListNode *prev=NULL, *current = head , *next = NULL;
+        
+        while (current!=NULL){
+            next = current->next;
+            current->next= prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
     }
-    head = prev;
+}
+
+
+struct ListNode * getTail(struct ListNode *cur){
+    while (cur!=NULL && cur->next!=NULL) {
+        cur = cur->next;
+    }
+    return cur;
+}
+
+struct ListNode *partitionList(struct ListNode *head, struct ListNode *end, struct ListNode **newHead, struct ListNode **newTail){
+    struct ListNode *pivot = end;
+    struct ListNode *prev = NULL , *cur = head, *tail = pivot;
+    
+    while (cur!= pivot) {
+        if (cur->data < pivot->data) {
+            if ((*newHead)==NULL) {
+                (*newHead) = cur;
+            }
+            prev = cur;
+            cur = cur->next;
+        }
+        else{
+            if (prev) {
+                prev->next = cur->next;
+            }
+            struct ListNode *tmp = cur->next;
+            cur->next = NULL;
+            tail->next = cur;
+            tail = cur;
+            cur = tmp;
+        }
+    }
+    if ((*newHead)==NULL) {
+        (*newHead) = pivot;
+    }
+    
+    (*newTail) = tail;
+    return pivot;
+}
+
+struct ListNode * quickSortRec(struct ListNode *head, struct ListNode *end){
+    if (!head && head==end) {
+        return head;
+    }
+    struct ListNode *newHead= NULL, *newTail = NULL;
+    struct ListNode *pivot = partitionList(head, end, &newHead, &newTail);
+    
+    if (newHead != pivot) {
+        struct ListNode *tmp = newHead;
+        while (tmp->next!=NULL) {
+            tmp = tmp->next;
+        }
+        tmp->next = NULL;
+        newHead = quickSortRec(newHead, tmp);
+        tmp = getTail(newHead);
+        tmp->next = pivot;
+    }
+    pivot->next = quickSortRec(pivot->next, newTail);
+    return newHead;
+}
+
+//here the sorting happens exclusive of the end node
+struct ListNode *quickSortRecur(struct ListNode *head, struct ListNode *end)
+{
+    // base condition
+    if (!head || head == end)
+        return head;
+    
+    struct ListNode *newHead = NULL, *newEnd = NULL;
+    
+    // Partition the list, newHead and newEnd will be updated
+    // by the partition function
+    struct ListNode *pivot = partitionList(head, end, &newHead, &newEnd);
+    
+    // If pivot is the smallest element - no need to recur for
+    // the left part.
+    if (newHead != pivot)
+    {
+        // Set the node before the pivot node as NULL
+        struct ListNode *tmp = newHead;
+        while (tmp->next != pivot)
+            tmp = tmp->next;
+        tmp->next = NULL;
+        
+        // Recur for the list before pivot
+        newHead = quickSortRecur(newHead, tmp);
+        
+        // Change next of last node of the left half to pivot
+        tmp = getTail(newHead);
+        tmp->next =  pivot;
+    }
+    
+    // Recur for the list after the pivot element
+    pivot->next = quickSortRecur(pivot->next, newEnd);
+    
+    return newHead;
+}
+
+void quickSortList(struct ListNode **head){
+    (*head) = quickSortRec(*head, getTail(*head));
+    return;
+}
+
+//Simple Sorting of linked List-------//
+void sortLinkedList(struct ListNode **headRef){
+    if ((*headRef)==NULL) {
+        return;
+    }
+    struct ListNode *temp = *headRef , *count = NULL;
+    for (; temp->next != NULL; temp=temp->next) {
+        for (count = temp->next; count!=NULL; count = count->next) {
+            if (temp->data > count->data) {
+                int t = temp->data;
+                temp->data = count->data;
+                count->data = t;
+            }
+        }
     }
 }
 
 void sortList(){
     if (!isListEmpty()) {
-        
+        sortLinkedList(&head);
+        // quickSortList(&head);
     }
 }
 
 
+#pragma mark- Quick Sort Array
+
+void swap(int *a, int *b){
+    int t =*a;
+    *a = *b;
+    *b = t;
+}
+
+void printArray(int arr[], int size){
+    for (int i=0; i<size; i++) {
+        printf("%d--",arr[i]);
+    }
+}
+
+int partition(int arr[], int start, int end){
+    int pivot = arr[end];
+    int pIndex = start;
+    for (int i= start; i<end; i++) {
+        if (pivot>=arr[i]) {
+            swap(&arr[pIndex], &arr[i]);
+            pIndex++;
+        }
+    }
+    swap(&arr[pIndex], &arr[end]);
+    return pIndex;
+}
+
+void quickSort(int arr[], int start, int end){
+    if (start<end) {
+        int partIndex = partition(arr, start, end);
+        quickSort(arr, start, partIndex-1);
+        quickSort(arr, partIndex+1, end);
+    }
+}
+
+void getMiddle(struct ListNode *headRef){
+    if (!isListEmpty()) {
+        struct ListNode *slowPtr = head , *fastPtr =head;
+        while (fastPtr!=NULL && fastPtr->next!=NULL) {
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+        }
+        printf("The Middle Node data is: %d",slowPtr->data);
+    }
+}
+
+void deleteLinkedList(struct ListNode **headRef){
+    if (!isListEmpty()) {
+        struct ListNode *current = *headRef, *next =NULL;
+        while (current!=NULL) {
+            next = current->next;
+            free(current);
+            current = next;
+        }
+        *headRef = NULL;
+    }
+}
+
+void detectLoopInList(struct ListNode *headRef){
+    if (!isListEmpty()) {
+        struct ListNode *sp = headRef, *fp = headRef;
+        while (sp && fp && fp->next!=NULL) {
+            sp= sp->next;
+            fp = fp->next->next;
+            if (sp == fp) {
+                printf("Found Loop in List");
+                break;
+            }
+        }
+    }
+}
+
+/* Outer loop is used to pick the elements one by one and inner loop compares the picked element with rest of the elements.
+ */
+void removeDuplicates(struct ListNode *headRef){
+    if (!isListEmpty()) {
+        struct ListNode *ptr1=headRef , *ptr2= NULL, *dup= NULL;
+        while (ptr1 != NULL && ptr1->next!=NULL) {
+            ptr2 = ptr1;
+            while (ptr2->next!=NULL) {
+                if (ptr1->data == ptr2->next->data) {
+                    dup = ptr2->next;
+                    ptr2->next = ptr2->next->next;
+                    free(dup);
+                }
+                else{
+                    ptr2= ptr2->next;
+                }
+            }
+            ptr1 = ptr1->next;
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
     
+    /*
+     //Quick Sort Array---------------//
+     int arr[] = {10, 80, 30, 90, 40, 50, 70};
+     int size = sizeof(arr)/sizeof(arr[0]);
+     quickSort(arr, 0, size-1 );
+     printArray(arr, size);
+     */
+    
     //Linked List Operations------------------------//
-
+    
     int choice = 0 ,num;
     while (1){
         printf("\nList Operations\n");
         printf("===============\n");
-        printf("1.Create List\n");
-        printf("2.Insert Node\n");
-        printf("3.Display\n");
-        printf("4.Insert at Position\n");
-        printf("5.Delete\n");
-        printf("6.Reverse List\n");
-        printf("7.Sort List\n");
-        printf("0.Exit\n");
+        printf("1.  Create List\n");
+        printf("2.  Insert Node\n");
+        printf("3.  Display\n");
+        printf("4.  Insert at Position\n");
+        printf("5.  Delete\n");
+        printf("6.  Reverse List\n");
+        printf("7.  Sort List\n");
+        printf("8.  Find Middle.\n");
+        printf("9.  Delete/ Clean Linked List.\n");
+        printf("10. Detect Loop in List.\n");
+        printf("11. Remove Duplicates.\n");
+        printf("0.  Exit.\n");
         printf("\n");
         printf("Enter your choice : ");
         scanf("%d",&choice);
@@ -176,19 +400,19 @@ int main(int argc, const char * argv[]) {
                 case 1:
                     createList();
                     break;
-                
+                    
                 case 2:{
                     printf("Enter Node Data:");
                     scanf("%d",&num);
                     insertNode(num);
                 }
                     break;
-                
+                    
                 case 3:{
                     displayList();
                 }
                     break;
-                
+                    
                 case 4:{
                     int position, data;
                     printf("Enter Position:");
@@ -221,7 +445,23 @@ int main(int argc, const char * argv[]) {
                     sortList();
                 }
                     break;
-                
+                case 8:{
+                    getMiddle(head);
+                }
+                    break;
+                case 9:
+                    deleteLinkedList(&head);
+                    break;
+                    
+                case 10:{
+                    //head->next->next->next = head;     //Creating  loop for testing----//
+                    detectLoopInList(head);
+                }
+                    break;
+                case 11:
+                    removeDuplicates(head);
+                    break;
+                    
                 default:
                     break;
             }
